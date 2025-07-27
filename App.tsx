5ee5646b6e29963aa1c55f1a1d2606b19a72f2c3
Rainbow1nTheDark <hai1rakegg3@gmail.com>
2025-07-27 13:40:55 -0400

    bug fix

--- App.tsx
+++ App.tsx
@@ -25,8 +25,8 @@ import { RobotIconV1 } from './components/icons/RobotIcon';
 import '@rainbow-me/rainbowkit/styles.css';
 import { polygon as wagmiPolygon } from 'viem/chains'; 
 
-const API_BASE_URL = 'http://localhost:3001/api';
-const SOCKET_SERVER_URL = 'http://localhost:3001';
+const API_BASE_URL = 'https://ratecaster.xyz/api';
+const SOCKET_SERVER_URL = 'https://ratecaster.xyz';
 const FRONTEND_RPC_URL = 'https://polygon-rpc.com'; 
 
 const queryClientTanstack = new QueryClient(); 
